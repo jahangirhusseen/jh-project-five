e573ce4cacb0f4416572ecd8a3571c73a6c2e5c5
Jahangir Hussen <ahmedjahangirid@gmail.com>
2025-09-04 18:14:15 +0600

    adeed Call Buttons  functionalities

--- js/script.js
+++ js/script.js
@@ -14,3 +14,54 @@ heartBtns.forEach((btn) => {
   btn.addEventListener("click", getHeartCount);
 });
 // ===========End Heat Counter =======================
+
+// ===================
+// Coin Count
+let coinCountValue = 100;
+const coinCount = document.querySelector("#coinCount");
+const coinBtns = document.querySelectorAll(".coinBtn");
+
+//Coin Count Feature
+const getCoinBtns = function (CardElement) {
+  let title = CardElement.querySelector(".card-body-title").innerText;
+  let subtitle = CardElement.querySelector(".subtitle").innerText;
+  let infoNumber = CardElement.querySelector(".info-number").innerText;
+  const time = new Date().toLocaleTimeString();
+
+  if (coinCountValue <= 0) {
+    alert("আপনার পর্যাপ্ত কয়েন নাই! কল করতে কমপক্ষে ২০ কয়েন লাগবে");
+    return;
+  }
+
+  coinCountValue -= 20;
+  coinCount.textContent = coinCountValue;
+
+  alert(`Calling ${subtitle} ${infoNumber}...`);
+
+  const data = {
+    title: title,
+    subtitle: subtitle,
+    infoNumber: infoNumber,
+    time: time,
+  };
+  historyData.push(data);
+  getHistoryData(data);
+};
+
+coinBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    let CardElement = btn.parentElement.parentElement;
+
+    getCoinBtns(CardElement);
+  });
+});
+
+// Clear Btn
+const clearBtn = document.querySelector("#clearBtn");
+
+clearBtn.addEventListener("click", function () {
+  historyData.length = 0;
+  console.log(historyData);
+
+  document.querySelector(".history-container").innerHTML = "";
+});
